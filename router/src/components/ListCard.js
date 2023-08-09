5c6f62b4bd285bf7fd5d2c94158dcdcfe65ac3c1
(jyh) <(jikkk9@naver.com)>
2023-08-09 16:29:16 +0900

    add ( 사진 출력까지 완료 )

--- router/src/components/ListCard.js
+++ router/src/components/ListCard.js
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 function ListCard({ item }) {
   return (
     <>
-      <div className='imgCon'></div>
+      <div className='imgCon'>
+        <img src={`${process.env.PUBLIC_URL}/img/${item.img} `}></img>
+      </div>
       <p>{item.title}</p>
       <p>{item.price}</p>
       <p>{item.category}</p>
