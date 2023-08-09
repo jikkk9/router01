7c28af7eea9c117b61e8b1e50758364444deca90
(jyh) <(jikkk9@naver.com)>
2023-08-09 11:13:20 +0900

    add import { Routes, Route, Link, useLocation, useNavigatem, Outlet } from 'react-router-dom'

--- router/src/App.js
+++ router/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './component/Header';
+import { Routes, Route, Link, useLocation, useNavigatem, Outlet } from 'react-router-dom'
 // npm install react-router-dom@6 으로설치
-
+// index.js에 < BrowserRouter >로 <app/>감싸고 
+// index.js에 import { BrowserRouter } from "react-router-dom"
 function App() {
   return (
     <div className="App">
-
+      <Header></Header>
     </div>
   );
 }
